c82706074774b19d158aa990299ee0dce7453ae5
aravinthAS <aravinth123@hotmail.com>
2018-06-02 19:03:47 +0200

    New test was added and some testvalue was modified

---
+++ Test/Exec_Script/Main/test_fault_tolerant.sh
@@ -0,0 +1,1 @@
+absc -erlang ../../Subway/Test_fault_tolerant.abs ../../../Receptionist/Receptionist.abs ../../../Stat/Stat.abs ../../../Client/Client.abs ../../../LoadBalancer/Load_balancer.abs ../../../Manager/Manager.abs ../../../Queue/Queue.abs ../../../Chef/Chef.abs ../../../Waiter/Waiter.abs ../../../Database/Db.abs ../../../Queue/DelayQueue.abs ../../../config.abs && ./gen/erl/run
